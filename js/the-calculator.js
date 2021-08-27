881eecb7611973c5edb522b04b1c84fa88caafdb
garytate123 <garytatelufc@gmail.com>
2021-08-27 15:24:35 +0100

    add half function

---
+++ js/the-calculator.js
@@ -0,0 +1,34 @@
+// Square function starts
+
+function squareNumber_1(){    
+    let num = prompt("Please enter a number to be squared.");
+    squareNumber(num);
+}
+
+function squareNumber(num){
+    let number = num;
+    let result = number * number;
+    document.getElementById("squareNumber").innerHTML = "The result of squaring the number " + number + " is " + result 
+}
+
+// Square function ends
+
+// Half Number function starts
+
+function halfNumber_1(){    
+    let num = prompt("Please enter a number that you would like to half.");
+    halfNumber(num);
+    // document.getElementById("halfNumber").innerHTML = "The result of halving the number " + halfNum + " is "; 
+}
+
+// I'm not sure why this function is working
+function halfNumber(num){
+    let number = Number.parseInt(num);
+    let resultHalf = Number.parseInt(number) / Number.parseInt(number);
+    document.getElementById("halfNumber").innerHTML = "The result of halving the number " + number + " is " + resultHalf; 
+}
+
+
+
+// Half Number function ends
+
