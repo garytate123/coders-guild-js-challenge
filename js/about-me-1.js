0fa77abc288dcaa7460b3ee04da5769d5890965d
garytate123 <garytatelufc@gmail.com>
2021-09-03 12:14:02 +0100

    add - about me task

---
+++ js/about-me-1.js
@@ -0,0 +1,25 @@
+// 4. Create a new img element and set its src attribute to a picture of you. 
+// Append that element to the body.
+
+let pageBody = document.getElementsByTagName("body")[0];
+pageBody.style.fontFamily = "Arial, sans-serif";
+pageBody.style.margin = "20px";
+
+document.getElementById("nickname").innerHTML = "Gary";
+document.getElementById("favorites").innerHTML = "Leeds United";
+document.getElementById("hometown").innerHTML = "Leeds";
+
+
+let listItems = document.getElementsByTagName("li");
+for (let i = 0; i < listItems.length; i++){
+    let list = listItems[i];
+    list.style
+    list.classList.add("listitem");
+} 
+
+let img = document.createElement("img");
+img.src = "https://www.castlegateit.co.uk/wp-content/uploads/2021/06/Gary-Tate-.jpg";
+img.style.border = '4px solid black';
+pageBody.appendChild(img);
+
+
