fc25d6c20f32dbb20065e7e51f7208cda3f7a98f
garytate123 <garytatelufc@gmail.com>
2021-09-03 12:45:33 +0100

    starter files for reading list part 2 and the counter function

---
+++ js/the-reading-list-part-2.js
@@ -0,0 +1,6 @@
+// 1. Create a webpage with an h1 of “My Book List”.
+// 2. Copy the array of books from the previous exercise.
+// 3. Iterate through the array of books. For each book, create a p element with the book title and author and append it to the page.
+// 4. Bonus: Use a ul and li to display the books.
+// 5. Bonus: add a property to each book with the URL of the book cover, and add an img element for each book on the page.
+// 6. Bonus: Change the style of the book depending on whether you have read it or not.
